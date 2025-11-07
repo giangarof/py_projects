530bfdd3713ec8133395d65232c231aa7b26ab75
Gianmarco Garofalo <giangarof98@gmail.com>
2025-11-07 18:21:34 -0500

    more challenges

--- challenges/challenge_9.py
+++ challenges/challenge_9.py
@@ -1,4 +1,13 @@
 # Write a function called countVowels that takes in a string and returns the number of vowels in the string.
 
-def countVowels():
-    pass
+
+def countVowels(word):
+    v = ["a", "e", "i", "o", "u"]
+    res = 0
+    for w in word:
+        if w in v:
+            res += 1
+    print(res)
+
+
+countVowels("javascript")
